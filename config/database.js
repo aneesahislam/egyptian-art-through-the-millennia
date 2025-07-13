f63064a74201b2b4e7e6538c1d9ef42d612276d1
Aneesah Islam <aneesah.s.islam@gmail.com>
2025-07-13 18:04:12 -0400

    updated

--- config/database.js
+++ config/database.js
@@ -1,9 +1,17 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb+srv://egyptianart:sE5tX4lDZ2Bq4BHi@cluster0.vwhb2fg.mongodb.net/egyptian-art', 
-    'dbName': 'egyptian-art'
+    'url' : 'mongodb+srv://aneesahsislam:4g33hN4FJQnvg8Xl@cluster0.kc2s9bo.mongodb.net', 
+    'dbName': 'ancient-egyptian-art'
 };
 
-//username: egyptianart
-//password: sE5tX4lDZ2Bq4BHi
+// username: ancient-egyptian-art
+// password: 4g33hN4FJQnvg8Xl
+
+
+// Old Username and Password
+// 'url' : 'mongodb+srv://egyptianart:sE5tX4lDZ2Bq4BHi@cluster0.vwhb2fg.mongodb.net/egyptian-art', 
+// 'dbName': 'egyptian-art'
+
+// username: egyptianart
+// password: sE5tX4lDZ2Bq4BHi
